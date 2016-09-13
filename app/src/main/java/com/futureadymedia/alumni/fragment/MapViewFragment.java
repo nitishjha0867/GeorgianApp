d01dea3d3ee69f1dd584199a6be1689b059c9e78
nitishjha0867 <nitishthespeedstar@gmail.com>
2016-09-13 12:41:35 +0530

    Map Integration

--- app/src/main/java/com/futureadymedia/alumni/fragment/MapViewFragment.java
+++ app/src/main/java/com/futureadymedia/alumni/fragment/MapViewFragment.java
@@ -1,15 +1,26 @@
 package com.futureadymedia.alumni.fragment;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v4.app.Fragment;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.animation.BounceInterpolator;
 import android.view.animation.Interpolator;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.futureadymedia.alumni.R;
@@ -21,17 +32,145 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by developer on 9/12/2016.
  */
-public class MapViewFragment extends Fragment implements GoogleMap.OnMarkerClickListener {
+public class MapViewFragment extends Fragment implements GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnInfoWindowClickListener,
+        GoogleMap.OnMarkerDragListener,
+        SeekBar.OnSeekBarChangeListener,
+        OnMapReadyCallback{
+
+    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+
+    private static final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
+
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+
+    private static final LatLng ADELAIDE = new LatLng(-34.92873, 138.59995);
+
+    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+
+    /** Demonstrates customizing the info window and/or its contents. */
+    class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+        // These are both viewgroups containing an ImageView with id "badge" and two TextViews with id
+        // "title" and "snippet".
+        private final View mWindow;
+
+        private final View mContents;
+
+        CustomInfoWindowAdapter() {
+            mWindow = getActivity().getLayoutInflater().inflate(R.layout.custom_info_window, null);
+            mContents = getActivity().getLayoutInflater().inflate(R.layout.custom_info_contents, null);
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            if (mOptions.getCheckedRadioButtonId() != R.id.custom_info_window) {
+                // This means that getInfoContents will be called.
+                return null;
+            }
+            render(marker, mWindow);
+            return mWindow;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            if (mOptions.getCheckedRadioButtonId() != R.id.custom_info_contents) {
+                // This means that the default info contents will be used.
+                return null;
+            }
+            render(marker, mContents);
+            return mContents;
+        }
+
+        private void render(Marker marker, View view) {
+            int badge;
+            // Use the equals() method on a Marker to check for equals.  Do not use ==.
+            if (marker.equals(mBrisbane)) {
+                badge = R.drawable.badge_qld;
+            } else if (marker.equals(mAdelaide)) {
+                badge = R.drawable.badge_sa;
+            } else if (marker.equals(mSydney)) {
+                badge = R.drawable.badge_nsw;
+            } else if (marker.equals(mMelbourne)) {
+                badge = R.drawable.badge_victoria;
+            } else if (marker.equals(mPerth)) {
+                badge = R.drawable.badge_wa;
+            } else {
+                // Passing 0 to setImageResource will clear the image view.
+                badge = 0;
+            }
+            ((ImageView) view.findViewById(R.id.badge)).setImageResource(badge);
+
+            String title = marker.getTitle();
+            TextView titleUi = ((TextView) view.findViewById(R.id.title));
+            if (title != null) {
+                // Spannable string allows us to edit the formatting of the text.
+                SpannableString titleText = new SpannableString(title);
+                titleText.setSpan(new ForegroundColorSpan(Color.RED), 0, titleText.length(), 0);
+                titleUi.setText(titleText);
+            } else {
+                titleUi.setText("");
+            }
+
+            String snippet = marker.getSnippet();
+            TextView snippetUi = ((TextView) view.findViewById(R.id.snippet));
+            if (snippet != null && snippet.length() > 12) {
+                SpannableString snippetText = new SpannableString(snippet);
+                snippetText.setSpan(new ForegroundColorSpan(Color.MAGENTA), 0, 10, 0);
+                snippetText.setSpan(new ForegroundColorSpan(Color.BLUE), 12, snippet.length(), 0);
+                snippetUi.setText(snippetText);
+            } else {
+                snippetUi.setText("");
+            }
+        }
+    }
 
     MapView mMapView;
-    private GoogleMap googleMap;
+
+    private GoogleMap mMap;
+
     private Marker myMarker;
+
+    private Marker mPerth;
+
+    private Marker mSydney;
+
+    private Marker mBrisbane;
+
+    private Marker mAdelaide;
+
+    private Marker mMelbourne;
+
+    /**
+     * Keeps track of the last selected marker (though it may no longer be selected).  This is
+     * useful for refreshing the info window.
+     */
+    private Marker mLastSelectedMarker;
+
+    private final List<Marker> mMarkerRainbow = new ArrayList<Marker>();
+
+    private TextView mTopText;
+
+    private TextView mTagText;
+
+    private SeekBar mRotationBar;
+
+    private CheckBox mFlatBox;
+
+    private RadioGroup mOptions;
+
+    private final Random mRandom = new Random();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,7 +179,34 @@ public class MapViewFragment extends Fragment implements GoogleMap.OnMarkerClick
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
 
-        mMapView.onResume(); // needed to get the map to display immediately
+       // needed to get the map to display immediately
+
+       /* if (mMapView.getViewTreeObserver().isAlive()) {
+            mMapView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @SuppressWarnings("deprecation") // We use the new method when supported
+                @SuppressLint("NewApi") // We check which build version we are using.
+                @Override
+                public void onGlobalLayout() {
+                    LatLngBounds bounds = new LatLngBounds.Builder()
+                            .include(PERTH)
+                            .include(SYDNEY)
+                            .include(ADELAIDE)
+                            .include(BRISBANE)
+                            .include(MELBOURNE)
+                            .build();
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                        mMapView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    } else {
+                        mMapView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+                }
+            });
+        }
+*/
+        mMapView.getMapAsync(this);
+
+        mMapView.onResume();
 
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
@@ -48,26 +214,109 @@ public class MapViewFragment extends Fragment implements GoogleMap.OnMarkerClick
             e.printStackTrace();
         }
 
-        mMapView.getMapAsync(new OnMapReadyCallback() {
+        return rootView;
+
+    }
+
             @Override
-            public void onMapReady(GoogleMap mMap) {
-                googleMap = mMap;
+            public void onMapReady(GoogleMap map) {
+                mMap = map;
+
+                Log.e("Map cy", "");
+
+                // Hide the zoom controls as the button panel will cover it.
+                /*mMap.getUiSettings().setZoomControlsEnabled(false);
+
+                // Add lots of markers to the map.
+                addMarkersToMap();
+
+                // Setting an info window adapter allows us to change the both the contents and look of the
+                // info window.
+                mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter());
+
+                // Set listeners for marker events.  See the bottom of this class for their behavior.
+                mMap.setOnMarkerClickListener(this);
+                mMap.setOnInfoWindowClickListener(this);
+                mMap.setOnMarkerDragListener(this);
+               /* mMap.setOnInfoWindowCloseListener(this);
+                mMap.setOnInfoWindowLongClickListener(this);*/
+
+                // Override the default content description on the view, for accessibility mode.
+                // Ideally this string would be localised.
+               /* map.setContentDescription("Map with lots of markers.");
+
+
 
                 // For showing a move to my location button
-                googleMap.setMyLocationEnabled(true);
+               // googleMap.setMyLocationEnabled(true);
 
                 // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-34, 151);
+                /*LatLng sydney = new LatLng(19.208740, 72.864526);
                 myMarker = googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
 
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            }
-        });
+               googleMap.setOnMarkerClickListener(this);
 
-        return rootView;
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(19).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));*/
+
+            }
+
+    private void addMarkersToMap() {
+        // Uses a colored icon.
+        mBrisbane = mMap.addMarker(new MarkerOptions()
+                .position(BRISBANE)
+                .title("Brisbane")
+                .snippet("Population: 2,074,200")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+        // Uses a custom icon with the info window popping out of the center of the icon.
+        mSydney = mMap.addMarker(new MarkerOptions()
+                .position(SYDNEY)
+                .title("Sydney")
+                .snippet("Population: 4,627,300")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.arrow))
+                .infoWindowAnchor(0.5f, 0.5f));
+
+        // Creates a draggable marker. Long press to drag.
+        mMelbourne = mMap.addMarker(new MarkerOptions()
+                .position(MELBOURNE)
+                .title("Melbourne")
+                .snippet("Population: 4,137,400")
+                .draggable(true));
+
+        // A few more markers for good measure.
+        mPerth = mMap.addMarker(new MarkerOptions()
+                .position(PERTH)
+                .title("Perth")
+                .snippet("Population: 1,738,800"));
+        mAdelaide = mMap.addMarker(new MarkerOptions()
+                .position(ADELAIDE)
+                .title("Adelaide")
+                .snippet("Population: 1,213,000"));
+
+        // Creates a marker rainbow demonstrating how to create default marker icons of different
+        // hues (colors).
+        float rotation = mRotationBar.getProgress();
+        boolean flat = mFlatBox.isChecked();
+
+        int numMarkersInRainbow = 12;
+        for (int i = 0; i < numMarkersInRainbow; i++) {
+            Marker marker = mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(
+                            -30 + 10 * Math.sin(i * Math.PI / (numMarkersInRainbow - 1)),
+                            135 - 10 * Math.cos(i * Math.PI / (numMarkersInRainbow - 1))))
+                    .title("Marker " + i)
+                    .icon(BitmapDescriptorFactory.defaultMarker(i * 360 / numMarkersInRainbow))
+                    .flat(flat)
+                    .rotation(rotation));
+            //marker.setTag(0);
+            mMarkerRainbow.add(marker);
+        }
     }
+
+
+
 
     @Override
     public void onResume() {
@@ -150,4 +399,39 @@ public class MapViewFragment extends Fragment implements GoogleMap.OnMarkerClick
     // marker is centered and for the marker's info window to open, if it has one).
     return false;
 }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
 }
